3d8513b41c28e216bbfcfddfb8e58e6aa4c5589b
monsteruser2021 <monsteruser2021@gmail.com>
2025-04-25 19:46:56 -0400

    Login

--- index.php
+++ index.php
@@ -1,5 +1,37 @@
-<?php
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
+    <!-- Styles -->
+     <link rel="stylesheet" href="styles/styles.css">
+  </head>
 
-echo "Bienvenido a la app";
-
-?>
+  <body>
+    <header>
+      <!-- place navbar here -->
+    </header>
+    <main>
+      <div class="container">
+        <div class="login-card">
+          <h5 class="login-title">Best login!</h5>
+          <div class="login-field">
+            <input type="text" class="login-input" name="username" id="username" placeholder="Username" >
+          </div>
+          <div class="login-field">
+            <input type="password" class="login-input" name="password" id="password" placeholder="Password" >
+          </div>
+          <button type="submit" class="login-btn">Login</button>
+        </div>
+      </div>
+    </main>
+    <footer>
+      <!-- place footer here -->
+    </footer>
+  </body>
+</html>
